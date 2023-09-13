90379a8bf3b49ba3b4ee15027a42903787c872fd
Paolla Marcel Mascarello Montes de Oca <steven.montesdeoca.noc3g@gmail.com>
2023-09-13 10:04:26 -0300

    Se corrige pronto para entregar!

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function descuentazo(n1) {
     let resultado = n1 * 0.8
     let total = n1 - resultado
-    alert("Excelente elección! En vez de pagar $" + n1 + " " + "Ahora solo deberá abonar la cómoda cantidad de: $" + total+"\nMuchas gracias por su compra!")
+    alert("Excelente elección!\nEn vez de pagar $" + n1 + " " + "Ahora solo deberá abonar la cómoda cantidad de: $" + total+"\nMuchas gracias por su compra!")
 }
 function premium(n1, n2, n3, n4, n5) {
     let prem = n1 + n2 + n3 + n4 + n5
@@ -21,7 +21,7 @@ alert("Bienvenido a Tiendas Dinamo!\nDonde tenemos la talla exacta para tu motor
 alert("Hoy es día de DESCUENTAZOS!!!\nY por ser el cliente número 81 en menos de 3 minutos usted ha ganado el DESCUENTAZO del 80%")
 
 do {
-    num1 = parseFloat(prompt("Por favor, elija una opción siendo:\n1: Dinamo\n2: Radiador\n3: juntas\n4: Motor\n5: Revision total del vehiculo\n6:Aumentar a Membresia premium"));
+    num1 = parseFloat(prompt("Por favor, elija una opción siendo:\n1: Dinamo\n2: Radiador\n3: Juntas\n4: Motor\n5: Revision total del vehiculo\n6:Aumentar a Membresia premium"));
     
     if (isNaN(num1) || num1 > 6 || num1 <= 0) {
         alert("Que te daban petróleo en la mamadera?\nElija del 1 al 6 el que desee.")
